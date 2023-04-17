6b2747fea62ecd0e5bf4f132c903f7211a2503f7
artur-nikiforenko@mail.ru <artur-nikiforenko@mail.ru>
2023-04-17 12:46:51 +0300

    Добавил восстановление пароля

--- Sports Nutrition Store Blazor/Data/UserService.cs
+++ Sports Nutrition Store Blazor/Data/UserService.cs
@@ -4,6 +4,7 @@
     {
         public string Login = string.Empty;
         public string Email = string.Empty;
+        public string RecoveryEmail = string.Empty;
         public string Password = string.Empty;
         public float Balance;
         public string Role  = string.Empty;
@@ -11,30 +12,34 @@
         public List<Product> Purchases = new List<Product>();
         public string FIO = string.Empty;
         public string PlaceOfWork = string.Empty;
+        public bool isAuth = false;
 
         public void SignIn(User user)
         {
-            if (user.Role == "Клиент")
+            if (isAuth == true)
             {
-                Login = user.Login;
-                Email = user.Email;
-                Password = user.Password;
-                Basket = user.Basket;
-                Role = user.Role;
-                Balance = user.Balance;
-                Purchases = user.Purchases;
+                if (user.Role == "Клиент")
+                {
+                    Login = user.Login;
+                    Email = user.Email;
+                    Password = user.Password;
+                    Basket = user.Basket;
+                    Role = user.Role;
+                    Balance = user.Balance;
+                    Purchases = user.Purchases;
+                }
+
+                else if (user.Role == "Админ")
+                {
+                    Login = user.Login;
+                    Email = user.Email;
+                    Password = user.Password;
+                    Role = user.Role;
+                    FIO = user.FIO;
+                    PlaceOfWork = user.PlaceOfWork;
+                }
             }
-            
-            else if (user.Role == "Админ")
-            {
-                Login = user.Login;
-                Email = user.Email;
-                Password = user.Password;
-                Role = user.Role;
-                FIO = user.FIO;
-                PlaceOfWork = user.PlaceOfWork;
-            }
-            
+            RecoveryEmail = user.Email;
             return;
         }
 
@@ -47,6 +52,12 @@
             Role = string.Empty;
             Balance = 0;
             Purchases = new List<Product>();
+            return;
+        }
+
+        public void ReturnLoginForRecovery(string Email)
+        {
+            this.Email = Email;
             return;
         }
     }
